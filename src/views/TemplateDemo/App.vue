22f3724a3c6f603fde797f323392d5acc25d1427
mataotao <mttwind@foxmail.com>
2020-08-23 17:24:48 +0800

    添加templatedemo

---
+++ src/views/TemplateDemo/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Demo />
+</template>
+
+<script>
+import Demo from "./Demo";
+export default {
+  name: "SlotUse",
+  components: {
+    Demo
+  }
+};
+</script>
+
+<style scoped></style>
