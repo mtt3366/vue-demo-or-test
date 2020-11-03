7ea57ab72dc524054e344c972406ce6cd278c74d
mataotao <mttwind@foxmail.com>
2020-11-03 10:08:01 +0800

    修改模板提示

--- src/views/TemplateDemo/App.vue
+++ src/views/TemplateDemo/App.vue
@@ -3,11 +3,11 @@
 </template>
 
 <script>
-import Demo from "./Demo";
+import Demo from "./Demo"; // todo
 export default {
-  name: "TemplateDemo",
+  name: "TemplateDemo", // todo
   components: {
-    Demo
+    Demo // todo
   }
 };
 </script>
--- src/views/TemplateDemo/Demo.vue
+++ src/views/TemplateDemo/Demo.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-  name: "Demo",
+  name: "Demo", // todo
   components: {}
 };
 </script>
